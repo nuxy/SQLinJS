8ed1b4531b2fbe306b1e6f07e6281ca87cfbcbc8
Marc S. Brooks <marc@mbrooks.info>
2014-03-08 14:51:49 -0800

    Updated SQL query and related QUnit assertion

--- test/select.js
+++ test/select.js
@@ -1,11 +1,11 @@
 asyncTest("Test method 'selectFrom'", function() {
-	var query = "SELECT * FROM user WHERE name != 'Marc' AND id > 2 AND id <= 9";
+	var query = "SELECT * FROM user WHERE name != 'Marc' AND id > 2 AND id != 3";
 
 	$this.SQLinJS('executeQuery', query,
 		function(res) {
 			ok(true, 'query executed: ' + query);
 			ok(res instanceof Array, 'response is Array');
-			ok(res.length == 7, res.length + ' of 7 results exist');
+			ok(res.length == 10, res.length + ' of 10 results exist');
 			start();
 		}
 	);
